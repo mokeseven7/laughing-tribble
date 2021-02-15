7c9428c9e6efebb5cc9e6015a4ca2eb23b06d199
Mike McGrath <mikemcgrath@Mikes-MacBook-Pro.local>
2021-02-15 01:05:30 -0700

    Initial commit

---
+++ lib/index.ts
@@ -0,0 +1,14 @@
+import * as cdk from '@aws-cdk/core';
+
+export interface ServerlessFatfreeProps {
+  // Define construct properties here
+}
+
+export class ServerlessFatfree extends cdk.Construct {
+
+  constructor(scope: cdk.Construct, id: string, props: ServerlessFatfreeProps = {}) {
+    super(scope, id);
+
+    // Define construct contents here
+  }
+}
